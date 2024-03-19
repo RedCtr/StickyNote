cfecb7e251e67089bd4be927ab0ccb7def8a0629
RedCtr <ridouanechatraoui03@gmail.com>
2024-03-19 02:24:47 +0000

    Revert "Configured Express with essential middlewares"
    This reverts commit 2022677b31df407e7b3a4b8983b8b7de75bd21b6.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,2 @@
-import express, { Express } from "express"
-import cors from "cors"
-import compression from "compression"
-import dotenv from "dotenv"
-
-dotenv.config()
-
-
-const app: Express = express()
-
-app.use(cors({
-    credentials: true
-}))
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(compression())
-
-const port = process.env.PORT || 4000
-
-
-app.listen(port, () =>
-    console.log(`Server is running on port ${port} `)
-)
 
 
