81b17862d8a92cce24a213172f515932298374b4
RedCtr <ridouanechatraoui03@gmail.com>
2024-03-20 14:40:25 +0000

    add all the crud functions in the note db file

--- server/src/db/notes.ts
+++ server/src/db/notes.ts
@@ -4,7 +4,8 @@ const NoteSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    updatedAt: { type: Date, default: Date.now },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 
@@ -16,3 +17,18 @@ NoteSchema.pre("save", function (next) {
     next()
 })
 export const NoteModel = mongoose.model("Note", NoteSchema)
+
+export const getNoteById = (noteId: string, userId: string) => NoteModel.findOne({ _id: noteId, user: userId })
+export const getAllNotesByUser = (userId: string) => NoteModel.find({ user: userId })
+export const createNote = (values: Record<string, any>) => new NoteModel(values).save().then((note) => note.toObject())
+export const updateNote = (noteId: string, userId: string, values: Record<string, any>) =>
+    NoteModel.findOneAndUpdate(
+        { _id: noteId, user: userId },
+        values,
+        { new: true }
+    )
+
+export const deleteNote = (noteId: string, userId: string) =>
+    NoteModel.findOneAndDelete(
+        { _id: noteId, user: userId }
+    )
