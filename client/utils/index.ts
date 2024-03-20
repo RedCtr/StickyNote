b26d443034724c6447512702cc5ebb7b66721a1d
RedCtr <ridouanechatraoui03@gmail.com>
2024-03-20 22:13:50 +0000

    refactor: remove access-control-origin header

--- client/utils/index.ts
+++ client/utils/index.ts
@@ -5,7 +5,6 @@ export const axiosInstance = axios.create({
     baseURL: 'http://localhost:4000',
     withCredentials: true,
     headers: {
-        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
     },
 })
