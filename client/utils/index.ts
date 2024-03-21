af738233f66127c4098d8d9d0e50a76c7a4f401c
RedCtr <ridouanechatraoui03@gmail.com>
2024-03-21 05:15:30 +0000

    fix: replace the railway BaseUrl with ec2 public url

--- client/utils/index.ts
+++ client/utils/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://note-server-production.up.railway.app',
+    baseURL: 'http://16.16.91.252',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
